faf96ee1e9a6798a47091cde212419c58e5bf3a3
infected521 <30352728+infected521@users.noreply.github.com>
2019-12-23 01:36:25 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -138,9 +138,9 @@ install_v2-ui() {
 }
 
 crt_key() {
-    $red
-    echo "\nInstalacion de certificado y key para v2ray\n"
-    echo "------------------------------------------\n"
+    printf "$red"
+    printf "\nInstalacion de certificado y key para v2ray\n"
+    printf "------------------------------------------\n"
     if [[ x"${release}" == x"centos" ]]; then
         yum install stunnel4 -y
     else
@@ -149,10 +149,10 @@ crt_key() {
        [[ ! -e /*.key ]] && read -p "Escribe el nombre del certificado (ejemplo:rock): " keyssl
        openssl genrsa -out /${keyssl}.key 2048
        openssl req -new -key /${keyssl}.key -x509 -days 1000 -out /${keyssl}.crt
-    echo "\n"
-    echo "agregar el directorio del certificado en el panel\n$yellow[certificate file path]$red /${keyssl}.crt\n"
-    echo "agregar el directorio de la key en el panel\n$yellow[key file path]$red /${keyssl}.key\n"
-    echo "------------------------------------------\n"
+    printf "\n"
+    printf "agregar el directorio del certificado en el panel\n$yellow[certificate file path]$red /${keyssl}.crt\n"
+    printf "agregar el directorio de la key en el panel\n$yellow[key file path]$red /${keyssl}.key\n"
+    printf "------------------------------------------\n"
 }
 
 echo -e "${green}Comience la instalación${plain}"
